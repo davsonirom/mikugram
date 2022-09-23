dd50e88e10c2261c2403dffa2f18c384162eed2d
Davson Oldac Damasceno Machado <machadinho@172.16.1.102>
2022-09-22 22:38:20 -0300

    espacamento entre os avatar

--- lib/main.dart
+++ lib/main.dart
@@ -202,82 +202,85 @@ class MeusInfluences extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(100),
-                color: Colors.red,
-                gradient: const LinearGradient(
-                  colors: [Colors.white, Colors.cyan],
-                  begin: Alignment.topCenter,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.red,
+                  gradient: const LinearGradient(
+                    colors: [Colors.white, Colors.cyan],
+                    begin: Alignment.topCenter,
+                  ),
+                ),
+                padding: const EdgeInsets.all(3),
+                height: cima,
+                width: lado,
+              ),
+              Container(
+                width: lado,
+                height: cima,
+                padding: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: CircleAvatar(
+                  backgroundImage: AssetImage(imagem),
                 ),
               ),
-              padding: const EdgeInsets.all(3),
-              height: cima,
-              width: lado,
-            ),
-            Container(
-              width: lado,
-              height: cima,
-              padding: const EdgeInsets.all(5),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(100),
-              ),
-              child: CircleAvatar(
-                backgroundImage: AssetImage(imagem),
-              ),
-            ),
-            SizedBox(
-              width: lado,
-              height: cima,
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 111, 177, 117),
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(2.0),
-                    child: Text(
-                      'AO VIVO',
-                      style: Theme.of(context).textTheme.headlineSmall,
+              SizedBox(
+                width: lado,
+                height: cima,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 111, 177, 117),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Text(
+                        'AO VIVO',
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              width: lado,
-              height: cima,
-              child: Align(
-                alignment: Alignment.bottomRight,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 111, 177, 117),
-                    borderRadius: BorderRadius.circular(100),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.all(.0),
-                    child: Icon(
-                      Icons.account_circle_outlined,
-                      size: 25,
-                      color: Colors.white,
+              SizedBox(
+                width: lado,
+                height: cima,
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 111, 177, 117),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(.0),
+                      child: Icon(
+                        Icons.account_circle_outlined,
+                        size: 25,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-        Text(
-          user,
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-      ],
+            ],
+          ),
+          Text(
+            user,
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ],
+      ),
     );
   }
 }
